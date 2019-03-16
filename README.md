afdd94dccc421e88e46588e690a70fae972f7480
Sally Beckwith <sallypbeckwith@gmail.com>
2019-03-16 15:58:42 -0400

    complete game with styling.

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# unit-4-game
+# unit-4-game (Treasure Game)
+In this game, the user is trying to get the total value to **exactly** equal the Goal value.
+The problem is that the values of each gem are unknown. Clicking on a gem will add its value 
+to the total.
+
+## For each round: 
+The goal value is randomly selected by the computer and will be a number between 19 and 120.
+Each gem is assigned a random value between 1 and 12. Some gems may have the same value.
+When the goal value is reached or exceeded, the round ends and the number of wins or losses is incremented.
+A new goal value is automatically chosen and a new round begins!
+
+##Additional feature:
+The user may select to work with anywhere from 2 to 6 gems. Changing the number of gems automatically starts a new round.
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,35 @@
 var jewels = [
   {
-    image: "./assets/images/bees.jpg",
+    image: "./assets/images/gem_1.png",
     value: 0,
   },
   {
-    image: "./assets/images/dilbert.jpg",
+    image: "./assets/images/gem_2.png",
     value: 0,
   },
   {
-    image: "./assets/images/nemo.jpg",
+    image: "./assets/images/gem_3.png",
     value: 0,
   },
   {
-    image: "./assets/images/bread.jpg",
+    image: "./assets/images/gem_4.png",
     value: 0,
   },
   {
-    image: "./assets/images/nemo.jpg",
+    image: "./assets/images/gem_5.png",
     value: 0,
   },
   {
-    image: "./assets/images/bread.jpg",
+    image: "./assets/images/gem_6.png",
     value: 0,
   },
 ]
-
+// ===== Variables =======================
 var goalNumber = 0;
-var totalNumber = 0;
+var total = 0;
+var gamesWon = 0;
+var gamesLost = 0;
+var maxJewels = 0;
 
 /**
  * Returns a random number between (and including) the two limits.
@@ -38,56 +41,81 @@ const getRandomNumber = (min,max) => {
     [min, max] = [max, min];  // swap the values if min > max
   }
   var numberSpan = max - min + 1;
-  value = Math.floor(Math.random()*numberSpan) + min;
-  console.log(value);
-  return value;
+  return Math.floor(Math.random()*numberSpan) + min;
 }
 
-const pickComputerNumber = () => {
+/**
+ * Generates goal number 
+ */
+const pickGoalNumber = () => {
   goalNumber = getRandomNumber(19,120);
 }
 
+/**
+ * Generates values for jewels
+ */
 const assignJewelvalues = () => {
-  jewels.forEach( (j) => {
-    j.value = getRandomNumber(1,12);
-  })
+  for (var j = 0; j < maxJewels; j++ ) {
+    jewels[j].value = getRandomNumber(1,12);
+  }
 }
 
+// ===== JQuery ============================
 $(document).ready(function() {
-
+  
   // When document is ready, insert jewels into the jewel-box div
-  // The number of jewels allowed is unlimited!
-  var jewelBox = $("#jewel-box");
-  for( var i = 0; i < jewels.length; i++) {
-    console.log('value is ' + jewels[i].value);
-    jewelBox.append(`<img id="jewel-${i}" class="w-1 p-1 jewel-img" 
-        src=${jewels[i].image} >`);
+  // The number of jewels can vary, so find which is selected
+  maxJewels = $("option").filter(":selected").text();
+
+  const fillJewelBox = () => {
+    var $jewelBox = $("#jewel-box");
+    $jewelBox.empty();
+    for( var i = 0; i < maxJewels; i++) {
+      $jewelBox.append(`<img id="jewel-${i}" class="p-2 gem-img" src=${jewels[i].image} >`);
+    }
   }
 
-  $("#goal-value").html(goalNumber)
+  /**
+   * Set inital values for a game. Reset the total to 0. Pick a new goal number. Determine the
+   * number of jewels to use and create them. Assign values to the jewels.
+   */
+  const initializeGame = () => {
+    total = 0;
+    pickGoalNumber();
+    fillJewelBox();
+    assignJewelvalues();
+    $("#goal-value").html(goalNumber);
+    $("#total-value").html(total);
+  }
 
-  // Use the last character(s) of id name to get the index to use for 
-  // the jewels array.  All id name begin with 'jewel-'
-  $(".jewel-img").on("click", function() {
-    var index = this.id.slice(6);
-    totalNumber += jewels[index].value;
-
-    // update calculatedNumber displayed
-    $("#total-value").html(totalNumber)
+  // Reset the game when the number of jewels in the jewel box changes
+  $("#num-jewels").on("change", () => {
+    maxJewels=$("option").filter(":selected").text();
+    initializeGame();
   })
 
+  // ===== Jewel click ===================
+  // Use the last character(s) of id name to get the index to use for 
+  // the jewels array.  All id names begin with 'jewel-'
+  $("#jewel-box").on("click", function(e) {
+    var index = e.target.id.slice(6);
+    total += jewels[index].value;
+    
+    // update calculatedNumber displayed
+    $("#total-value").html(total)
+    // check for end of game
+    if (total === goalNumber) {
+      gamesWon++;
+      $("#wins-value").html(gamesWon.toString());
+      initializeGame();
+    } else if(total > goalNumber) {
+      gamesLost++;
+      $("#losses-value").html(gamesLost.toString());
+      initializeGame();
+    }
+  })
 
-
-
+  // ===== BEGIN GAME ====================
+  initializeGame();
 })
 
-// ========================  Execute code
-pickComputerNumber();
-assignJewelvalues();
-
-//===============================================  SAMPLE JQuery
-// $(".operator").on("click", function() {
-//   isOperator = !isOperator;
-//   operator = $(this).val();
-//   $("#operator").text(symbols[operator]);
-// })
